64e8312aa2e62c651085de8e965c2dca3082fbc1
Amir SeerSol <amirgee007@yahoo.com>
2018-07-24 17:06:09 +0500

    getters

--- src/components/Result.vue
+++ src/components/Result.vue
@@ -1,6 +1,10 @@
 <template>
 <div>
     <p>Counter is: {{ counter }}</p>
+    <p>Counter Double: {{ counter2 }}</p>
+    <p>Clicks of Numbers: {{ clicks }}</p>
+
+
 </div>
 </template>
 
@@ -9,6 +13,12 @@
         computed: {
             counter(){
                 return this.$store.state.counter;
+            },
+            counter2(){
+                return this.$store.getters.doubleCounter;
+            },
+            clicks(){
+                return this.$store.getters.stringCounter;
             }
         }
     }
